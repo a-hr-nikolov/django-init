24ad2175240da696a091f240df73e129a28a5419
Atanas Nikolov <a.hr.nikolov@gmail.com>
2024-11-04 07:16:31 +0200

    Update user tests to use pytest

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,28 +1,28 @@
 import pytest
 from django.core.exceptions import ValidationError
-from django.test import TestCase
 
 from apps.users.models import BaseUser
 from apps.users.services import user_create
 
 
-class UserCreateTests(TestCase):
-    def test_user_without_password_is_created_with_unusable_one(self):
-        user = user_create(email="random_user@hacksoft.io")
+@pytest.fixture
+def user(db) -> BaseUser:
+    return user_create(email="john_doe@example.com")
 
-        self.assertFalse(user.has_usable_password())
 
-    def test_user_with_capitalized_email_cannot_be_created(self):
-        user_create(email="random_user@hacksoft.io")
-
-        with self.assertRaises(ValidationError):
-            user_create(email="RANDOM_user@hacksoft.io")
-
-        self.assertEqual(1, BaseUser.objects.count())
+def test_user_without_password_is_created_with_unusable_one(user: BaseUser):
+    assert not user.has_usable_password()
 
 
 @pytest.mark.django_db
-def test_user_without_password_is_created_with_unusable_one():
-    user = user_create(email="random_user@hacksoft.io")
+def test_user_with_capitalized_email_cannot_be_created_if_uncapitalized_exists(
+    user: BaseUser,
+):
+    email = user.email
+    email_parts = email.split("@")
+    email_parts[0] = email_parts[0].upper()
+    email = "@".join(email_parts)
+    with pytest.raises(ValidationError):
+        user_create(email=email)
 
-    assert not user.has_usable_password()
+    assert BaseUser.objects.count() == 1
